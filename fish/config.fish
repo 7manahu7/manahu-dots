ee8aa5f06e4cacee4567e816ee87943814354fcc
7MANAHU7 <umeshaditya1214@gmail.com>
2026-04-15 10:29:28 +0530

    clean dots

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+oh-my-posh init fish --config ~/posh-themes/oh-my-posh/themes/blueish.omp.json | source
